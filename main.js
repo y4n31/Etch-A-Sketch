fef4ae2419d95d7ccb3a6765f471655aa6bef53f
y4n31 <91342617+y4n31@users.noreply.github.com>
2024-04-27 14:07:27 -0500

    created function to append cells to the page

--- main.js
+++ main.js
@@ -2,7 +2,11 @@ const container = document.querySelector('.container');
 
 function gridCreation(numOfCells) {
     for(let i = 0; i < numOfCells; i++) {
-        const row = document.createElement('div');
-        
+        for(let j = 0; j < numOfCells; j++) {
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            container.appendChild(cell);
+        }
     }
 }
+gridCreation(4);
