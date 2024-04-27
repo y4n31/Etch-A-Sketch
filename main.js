89cecda8470dab5d35f5c7d3628bd933e7066480
y4n31 <91342617+y4n31@users.noreply.github.com>
2024-04-27 13:54:11 -0500

    located container in js and styled it

---
+++ main.js
@@ -0,0 +1,8 @@
+const container = document.querySelector('.container');
+
+function gridCreation(numOfCells) {
+    for(let i = 0; i < numOfCells; i++) {
+        const row = document.createElement('div');
+        
+    }
+}
