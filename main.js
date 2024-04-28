b810e200a5a1564bc5cdf512ed2e0a7d7eef3fa4
y4n31 <91342617+y4n31@users.noreply.github.com>
2024-04-28 16:48:48 -0500

    added code to paint divs when hovering over them

--- main.js
+++ main.js
@@ -5,11 +5,17 @@ function gridCreation(numOfCells) {
         const column = document.createElement('div');
         column.classList.add('column');
         for(let j = 0; j < numOfCells; j++) {
-            const row = document.createElement('div');
-            row.classList.add('row');
-            column.appendChild(row);
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            column.appendChild(cell);
+
+            cell.addEventListener('mouseover', function() {
+                cell.style.backgroundColor = 'red';
+            });
         }
-        container.appendChild(column)
+        container.appendChild(column);
     }
+
+    
 }
 gridCreation(16);
