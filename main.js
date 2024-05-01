b747dfa4d77352ce3ca507c56bccb2f12bb763c4
y4n31 <91342617+y4n31@users.noreply.github.com>
2024-05-01 15:10:31 -0500

    added button and eventlistener to create new grid currently it adds new grid to container with the existing grid

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
 const container = document.querySelector('.container');
+const body = document.querySelector('body');
+const button = document.createElement('button');
+
+button.innerText = 'Create a new grid';
+button.setAttribute('style', 'margin-bottom: 10px;')
+body.appendChild(button);
+
+button.addEventListener('click', () => {
+    gridCreation(0);
+    gridCreation(prompt('Choose number of blocks'));
+});
 
 function gridCreation(numOfCells) {
     for(let i = 0; i < numOfCells; i++) {
@@ -8,14 +19,13 @@ function gridCreation(numOfCells) {
             const cell = document.createElement('div');
             cell.classList.add('cell');
             column.appendChild(cell);
-
+            
             cell.addEventListener('mouseover', function() {
                 cell.style.backgroundColor = 'red';
             });
         }
         container.appendChild(column);
     }
-
-    
 }
-gridCreation(16);
+//gridCreation(16);
+
